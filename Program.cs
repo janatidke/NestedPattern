4ce0b9d3c1514feb0dbf655aac4ce871c6aeedc9
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-08-31 22:41:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NestedPattern
+{
+    class Program
+    {
+      
+            static void Main(string[] args)
+            {
+
+                for (int i = 1; i <= 5; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write(j.ToString());
+                    }
+                    Console.WriteLine();
+                }
+
+
+            }
+
+
+        }
+
+    }
+
+
